8fabadce9bd4f8bf7b8e0e68cdff26a2ecd4112c
Danil Karnovich <KarnovichDanil@gmail.com>
2023-12-02 21:45:14 +1000

    Code refactoring

--- webscrapping.py
+++ webscrapping.py
@@ -1,27 +1,23 @@
 from bs4 import BeautifulSoup
 import requests
-links = []
-value = []
-headers = {
-    'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'
-}
 
-proxies = {
-    'https': 'http://185.191.236.162:3128'
-}
 
-url = 'https://www.cian.ru/kupit-kvartiru/'
-request = requests.get(url)
-bs = BeautifulSoup(request.text, 'html.parser')
-all_links = bs.find_all('a', class_='_93444fe79c--link--VtWj6')
-for links in all_links:
-    links.append(links['href'])
-print(links)
+def main():
+    url = 'https://www.cian.ru/kupit-kvartiru/'
 
-for link in links:
-    value.append(requests.get(link))
+    request = requests.get(url)
+    soup = BeautifulSoup(request.text, 'html.parser')
 
-pbs = BeautifulSoup(value.text, 'html.parser')
-all_prices = pbs.find_all('span', class_='a10a3f92e9--lineHeight_9u--limEs')
-for price in all_prices:
-    print(price.text)
+    links = [item['href'] for item in soup.find_all('a', class_='_93444fe79c--link--VtWj6')]
+    with open('links.txt', 'w') as file:
+        for link in links:
+            file.write(link+'\n')
+
+    prices = [item.text.replace(' ', '_')[:-2] for item in soup.find_all('span', {'data-mark':'MainPrice'})]
+    with open('prices.txt', 'w') as file:
+        for price in prices:
+            file.write(price+'\n')
+
+
+if __name__ == '__main__':
+    main()
