e5d988739da838292c10cead8f05dadbd071e543
rodof-collab <rodofatos71@gmail.com>
2023-12-02 23:16:06 +1000

    added_square_price_and_districts

--- webscrapping.py
+++ webscrapping.py
@@ -9,15 +9,24 @@ def main():
     soup = BeautifulSoup(request.text, 'html.parser')
 
     links = [item['href'] for item in soup.find_all('a', class_='_93444fe79c--link--VtWj6')]
-    with open('links.txt', 'w') as file:
+    with open('sub\\links.txt', 'w') as file:
         for link in links:
             file.write(link+'\n')
 
     prices = [item.text.replace(' ', '_')[:-2] for item in soup.find_all('span', {'data-mark':'MainPrice'})]
-    with open('prices.txt', 'w') as file:
+    with open('sub\\prices.txt', 'w') as file:
         for price in prices:
             file.write(price+'\n')
+    sqprices = [item.text.replace(' ', '_')[:-4] for item in soup.find_all('p', {'data-mark':'PriceInfo'})]
+    with open('sub\\sqprices.txt', 'w', encoding='utf-8') as file:
+        for sqprice in sqprices:
+            file.write(sqprice+'\n')
+    districts = [item.text for item in soup.find_all('div', {'class':'_93444fe79c--labels--L8WyJ'})]
+    with open ('sub\\districts.txt', 'w', encoding='utf-8') as file:
+        for district in districts:
+            file.write(district+'\n',)
 
 
 if __name__ == '__main__':
     main()
+    
