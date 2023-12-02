27d59e713a74cdba0c54d821515fac76dd0aa9f2
rodof-collab <rodofatos71@gmail.com>
2023-12-02 19:37:57 +1000

    dva

--- webscrapping.py
+++ webscrapping.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
+links = []
+value = []
 headers = {
     'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'
 }
@@ -12,8 +14,14 @@ url = 'https://www.cian.ru/kupit-kvartiru/'
 request = requests.get(url)
 bs = BeautifulSoup(request.text, 'html.parser')
 all_links = bs.find_all('a', class_='_93444fe79c--link--VtWj6')
-all_prices = bs.find_all("span", class_='')
-for link in all_links:
-    print(link['href'])
+for links in all_links:
+    links.append(links['href'])
+print(links)
+
+for link in links:
+    value.append(requests.get(link))
+
+pbs = BeautifulSoup(value.text, 'html.parser')
+all_prices = pbs.find_all('span', class_='a10a3f92e9--lineHeight_9u--limEs')
 for price in all_prices:
     print(price.text)
